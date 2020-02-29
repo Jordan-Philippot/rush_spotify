d11ffb0914291dd15f49ecc0eb8753cea9c5d82b
Zach <pro.bizach@gmail.com>
2020-02-29 19:54:32 +0100

    Album done

--- api/model.class.php
+++ api/model.class.php
@@ -10,9 +10,31 @@ class Model extends DatabaseConn
     //All our requests to the database
     public function getAllAlbums()
     {
-        $sql = "SELECT * FROM albums
+        $sql = "SELECT *, albums.name album_name
+        FROM albums
         left join tracks on albums.id = tracks.album_id";
         $statement = $this->db->prepare($sql);
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+    public function getAlbum(string $name)
+    {
+        $sql = "SELECT *, albums.name album_name
+        FROM albums
+        left join tracks on albums.id = tracks.album_id
+        where albums.name like '%:name%'";
+        $statement = $this->db->prepare($sql);
+        $statement->bindParam(':name', $name);
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+    public function getArtist(string $name)
+    {
+        $sql = "SELECT *, albums.name album_name, albums.id album_id from artists
+        INNER join albums on artists.id = albums.artist_id
+        where artists.name like '%:name%'";
+        $statement = $this->db->prepare($sql);
+        $statement->bindParam(':name', $name);
         $statement->execute();
         return $statement->fetchAll();
     }
