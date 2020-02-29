3f581db900d4dd822db587957ce7376ff8b72383
Jordan <jordan.philippot@epitech.eu>
2020-03-01 00:14:00 +0100

    Added All previous work

--- dev/src/App.js
+++ dev/src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
-
+import React from 'react';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 function App() {
-  return <div className="App">khjkjhjhlkhjklhlkjhkljhl</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
