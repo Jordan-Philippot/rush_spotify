aefbf7b2821618c8920461b0105f465a00eabdf2
Jordan <jordan.philippot@epitech.eu>
2020-03-01 22:47:48 +0100

    BannerGender

--- dev/src/components/BannerGenders.js
+++ dev/src/components/BannerGenders.js
@@ -8,10 +8,10 @@ export default function GendersBanner(props) {
     }
     return (
 
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 gender-component" onClick={() => albumgender(props.gender.id)}>
-            <div class="offset-xl-1 col-xl-10">
-                <div class="gender">
-                    <div class="album-text">
+        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 gender-component" onClick={() => albumgender(props.gender.id)}>
+            <div className="offset-xl-1 col-xl-10">
+                <div className="gender">
+                    <div className="album-text">
                         <h5>{props.gender.name}</h5>
                     </div>
                 </div>
