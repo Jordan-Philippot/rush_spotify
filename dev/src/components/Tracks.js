1e24aa51497f75f5432966f1b1ad8513377a69a8
Belinda Boudra <belinda.boudra@epitech.eu>
2020-03-01 16:36:55 +0100

    Done with formating

--- dev/src/components/Tracks.js
+++ dev/src/components/Tracks.js
@@ -1,26 +1,26 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Tracks() {
     const [tracks, settracks] = useState(null)
-   
-    useEffect(()=>{
+
+    useEffect(() => {
         fetch("http://localhost:8080/tracks")
-        .then(response => response.json())
-        .then(json => settracks(json));
-    },[])
+            .then(response => response.json())
+            .then(json => settracks(json));
+    }, [])
     return (
         <div className="container">
-            {tracks === null ? "chargement..." : 
-            tracks.data.map(track => <div className="track">
-                <figure><figcaption>{track.name}</figcaption>
-                <audio
-                    controls
-                    src={track.mp3}>
-                </audio>
-                </figure>
-                
-            </div>
-            ) }
+            {tracks === null ? "chargement..." :
+                tracks.data.map(track => <div className="track">
+                    <figure><figcaption>{track.name}</figcaption>
+                        <audio
+                            controls
+                            src={track.mp3}>
+                        </audio>
+                    </figure>
+
+                </div>
+                )}
         </div>
     )
 }
